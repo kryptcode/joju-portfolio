679ee31ea8ff1ab058c289bcef14ab2afa4e6b81
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-03-26 21:08:51 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,51 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Joju</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <h2 className='w-[90%] mx-auto py-5 text-3xl font-bold tracking-wider mb-8'>
+        Joju
+      </h2>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      {/* Showcase */}
+      <div className='pb-4 mb-5'>
+        <div className='flex w-[96%] space-x-4 sm:w-[90%] mx-auto'>
+            <div className='flex-1 mt-8 sm:mt-24'>
+            <img src="https://i.ibb.co/QJR0kp8/mCD.png" alt="" className='w-[100%]' />
+          </div>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className='flex-1 relative space-y-3'>
+            <img src="./images/SKK.png" alt="" className='w-[100%]' />
+            <div className='flex space-x-2'>
+            <img src="./images/SSV1BLK.png" alt="" className='w-[50%]' />
+            <img src="./images/SSV1WT.png" alt="" className='w-[50%]' />
+            </div>
+          </div>
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className='flex w-[96%] items-center sm:w-[90%] mx-auto'>
+          <div>
+            <img src="./images/AQUAMOTION.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      {/* Contact */}
+      <div className='w-[96%] sm:w-[90%] mx-auto mb-10'>
+        <h5 className='font-semibold text-xl mb-6'>
+          Contact
+        </h5>
+        <p className='mb-2 text-gray-400'>
+          General Enquiries
+        </p>
+        <a href="mailto:adet@gmail.com">taiye'semail</a>
+      </div>
+
+
     </div>
   )
-}
+};
