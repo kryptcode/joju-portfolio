37aed3cf9cceeb54203bc72f08a6c5a2a486aa8c
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-03-27 15:08:30 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -47,9 +47,69 @@ const Home = () => {
         <a href="mailto:adeyombotk@gmail.com">adeyombotk@gmail.com</a>
       </div>
 
-      <h3>
+      <h3 className='w-[96%] sm:w-[90%] mx-auto text-2xl font-semibold mb-16'>
         Work Archive 2020 - Present
       </h3>
+
+      {/* Projects */}
+      <div className='w-[96%] sm:w-[90%] mx-auto '>
+        <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-5 items-center mb-4'>
+          <div className='flex-1'>
+            <img src="./images/AQUAMOTION.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>
+              Aquamotion
+            </p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/final rollout.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>
+              Twenties Merch
+            </p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/THE SACRED FEMININE BG.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>The Sacred Feminine</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/SKK.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>Eccentrist</p>
+          </div>
+        </div>
+
+        <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-5 items-center mb-4'>
+          <div className='flex-1'>
+            <img src="https://i.ibb.co/QJR0kp8/mCD.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>MicroDosing</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/LISSOME STATIONERY.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>Lissome</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/LISSOME PAPER BAGS.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>Lissome Paper Bags</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/TIRED YUTE WHITE BG.png" alt="" className='w-[100%] mb-4' />
+            <p className='text-gray-300 text-sm'>Tired Yute</p>
+          </div>
+        </div>
+
+        <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-5 items-center mb-4'>
+          <div className='flex-1'>
+            <img src="./images/SWISH-EMBLEM-BACK-BG.png" alt="" className='w-[100%] mb-4' />
+            <p className="text-gray-300 text-sm">Swish Back</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/SWISH-EMBLEM-FRONT-BG.png" alt="" className='w-[100%] mb-4' />
+            <p className="text-gray-300 text-sm">Swish Front</p>
+          </div>
+          <div className='flex-1'>
+            <img src="./images/SSV1BLK.png" alt="" className='w-[100%] mb-4' />
+            <p className="text-gray-300 text-sm">Safespace Blk</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
